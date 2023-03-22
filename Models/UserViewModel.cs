342cfc62b429101fe0e9be75d07746f13a9375de
Evandru <evandro_remolli.t@outlook.com>
2023-03-22 19:53:04 -0300

    Organização das Pastas

--- Models/UserViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AprendendoMVC.Models
-{
-    public class UserViewModel
-    {
-        public string Username { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
